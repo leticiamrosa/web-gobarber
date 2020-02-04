1d3e5219359218337675f74b3c5370b44be2a5cd
leticiamrosa <leticia@vizir.com.br>
2020-02-04 16:08:02 -0300

    (feat): implementation root import

--- app/src/pages/_layouts/default/styles.js
+++ app/src/pages/_layouts/default/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  height: 100vh;
   background: #333;
 `;
---
+++ app/src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+  }
+
+`;
