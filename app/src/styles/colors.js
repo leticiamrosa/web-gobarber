4f9d158b2285f64e42c86d57223cc5a3a8396e65
Leticia Monteiro <leticiamac@MacPro-leticia.local>
2020-03-16 09:56:32 -0300

    feat(header): implementation notification feedback

---
+++ app/src/styles/colors.js
@@ -0,0 +1,21 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  primary: {
+    main: '#7159c1',
+    variant: '#ab59c1',
+  },
+  secondary: {
+    main: '#3b9eff',
+    variant: '#ff892e',
+  },
+
+  grey300: '#eee',
+  grey400: '#999',
+  grey500: '#333',
+
+  error: '#fb6f91',
+};
+
+export default colors;
